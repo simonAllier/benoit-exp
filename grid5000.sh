953b606b913debf86fa5937b02a347454eb6c57a
Simon Allier <simon.allier@gmail.com>
2015-03-26 10:59:55 +0100

    fix script

--- grid5000.sh
+++ grid5000.sh
@@ -6,7 +6,7 @@ mvn clean -Dmaven.test.skip=true package
 
 rm -rf repo
 
-java -Dhttp.proxyHost=proxy.rennes.grid5000.fr -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr -Dhttp.proxyPort=3128 -cp /target/benoit-exp-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.benoit.Main init
+java -Dhttp.proxyHost=proxy.rennes.grid5000.fr -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr -Dhttp.proxyPort=3128 -cp target/benoit-exp-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.benoit.Main init
 
 
 cpu=$(cat nbProcess)
@@ -14,7 +14,7 @@ for i in `seq 1 $1`
 do
     for j in `seq 1 $cpu`
     do
-        java  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -cp /target/benoit-exp-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.benoit.Main &
+        java  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -cp target/benoit-exp-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.benoit.Main &
     done
     wait
     killall java
