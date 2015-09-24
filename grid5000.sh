7ec1827a2048032b9a5fa6654c361c2a3b3e8eab
Simon Allier <simon.allier@gmail.com>
2015-09-24 15:59:48 +0200

    update

--- grid5000.sh
+++ grid5000.sh
@@ -6,17 +6,7 @@ mvn clean -Dmaven.test.skip=true package
 
 rm -rf results
 
-java -Dhttp.proxyHost=proxy.rennes.grid5000.fr -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr -Dhttp.proxyPort=3128 -cp target/benoit-exp-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.benoit.Main init
 
+java  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -cp target/benoit-exp-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.benoit.Main
 
-cpu=$(cat nbProcess)
-for i in `seq 1 100000`
-do
-    for j in `seq 1 $cpu`
-    do
-        java  -Dhttp.proxyHost=proxy.rennes.grid5000.fr  -Dhttps.proxyPort=3128 -Dhttps.proxyHost=proxy.rennes.grid5000.fr  -Dhttp.proxyPort=3128  -cp target/benoit-exp-1.0-SNAPSHOT-jar-with-dependencies.jar fr.inria.benoit.Main &
-    done
-    wait
-    killall java
-done
 
--- src/main/java/fr/inria/benoit/Main.java
+++ src/main/java/fr/inria/benoit/Main.java
@@ -8,51 +8,63 @@ import java.util.*;
 
 import java.io.*;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Main program
  */
 public class Main {
 
+    private int nbProcessor;
+
     public static void main(String[] args) throws Exception {
+        Main main = new Main();
+        main.initNbOfProcess();
+        main.initGit();
 
-        if(args.length != 0) {
-            try {
-                initNbOfProcess();
-                initGit();
-            } catch (Exception e) {
-                Log.error("Main ", e);
-            }
-        }
-        else {
-            runExp();
-        }
+        main.runExp();
     }
 
-    protected static void runExp() throws IOException, InterruptedException, GitAPIException {
+    protected  void runExp() throws InterruptedException, GitAPIException, IOException {
         while (true) {
-            try {
-                List<String> params = initParameter();
-                
-                for(String param : params) {
-                    Log.info("run: {}", "sh bin/run_simus.sh /opt/mcr/v80/ " + param);
-                    Process p = Runtime.getRuntime().exec("sh bin/run_simus.sh /opt/mcr/v80/ " + param);
-                    p.waitFor();
-                }
-                
-                GitUtils gitUtils = new GitUtils("exp");
-                gitUtils.pull();
-                Log.info("push result");
-                gitUtils.add("results");
-                gitUtils.commit("update");
-                gitUtils.push();
-                
-            } catch (Throwable e) {}
+            ExecutorService executor = Executors.newFixedThreadPool(nbProcessor);
+            List<String> params = initParameter();
+
+            for(String p : params) {
+                final String param = p;
+                Runnable runnable = new Runnable() {
+                    public void run() {
+                        Log.info("run: {}", "sh bin/run_simus.sh /opt/mcr/v80/ " + param);
+                        try {
+                            Process p = Runtime.getRuntime().exec("sh bin/run_simus.sh /opt/mcr/v80/ " + param);
+                            p.waitFor();
+
+                            GitUtils gitUtils = new GitUtils("exp");
+                            gitUtils.pull();
+                            Log.info("push result");
+                            gitUtils.add("results");
+                            gitUtils.commit("update");
+                            gitUtils.push();
+                        } catch (InterruptedException e ) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (GitAPIException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                executor.execute(runnable);
+            }
+            executor.shutdown();
+            while (!executor.isTerminated()) {   }
         }
 
     }
 
-    protected static void initGit() throws IOException, GitAPIException {
+    protected void initGit() throws IOException, GitAPIException {
         Log.info("clone the repository https://github.com/simonAllier/benoit-exp-result.git");
         GitUtils gitUtils = new GitUtils("exp");
 
@@ -64,7 +76,7 @@ public class Main {
         }
     }
 
-    protected static List<String> initParameter() throws InterruptedException, IOException, GitAPIException {
+    protected List<String> initParameter() throws InterruptedException, IOException, GitAPIException {
         GitUtils gitUtils = new GitUtils("exp");
 
         Random r = new Random();
@@ -72,26 +84,26 @@ public class Main {
         Log.info("sleep {} seconds", sleep);
         Thread.sleep(sleep * 1000);
         gitUtils.pull();
-        return gitUtils.getPropertiesFiles(10);
+        return gitUtils.getPropertiesFiles(10*nbProcessor);
     }
 
 
-    protected static void initNbOfProcess() throws InterruptedException, IOException {
+    protected void initNbOfProcess() throws InterruptedException, IOException {
         Runtime r = Runtime.getRuntime();
 
         Process p = r.exec("cat /proc/cpuinfo");
         p.waitFor();
         BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line;
-        int i = 0;
         while ((line = reader.readLine()) != null) {
             if(line.startsWith("processor"))
-                i++;
+                nbProcessor++;
+        }
+        if(nbProcessor == 0) {
+            nbProcessor = 1;
         }
         reader.close();
-        BufferedWriter out = new BufferedWriter(new FileWriter("nbProcess"));
-        out.write(i+"");
-        out.close();
+
     }
 
 }
